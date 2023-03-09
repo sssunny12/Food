f80fd73d592cd490d9d73fc4e891d98066baba1f
Praveen <sssunny12@gmail.com>
2023-03-09 12:52:18 +0530

    worked on reports and dashboard

--- datasets/sample.sql
+++ datasets/sample.sql
@@ -7,27 +7,29 @@ CREATE TABLE "Account" (
 	"ParentId" VARCHAR(255), 
 	PRIMARY KEY (id)
 );
-INSERT INTO "Account" VALUES(1,'','Faith Santos DDS','','');
-INSERT INTO "Account" VALUES(2,'','Mr. Elijah Herrera DVM','','');
-INSERT INTO "Account" VALUES(3,'','Ms. Debra Jacobson PhD','','');
-INSERT INTO "Account" VALUES(4,'','Tricia Melendez','','');
-INSERT INTO "Account" VALUES(5,'','Elizabeth Barajas DDS','','');
-INSERT INTO "Account" VALUES(6,'','Mrs. Rose Barnett DDS','','');
-INSERT INTO "Account" VALUES(7,'','Mr. Travis Taylor II','','');
-INSERT INTO "Account" VALUES(8,'','Fernando Navarro DDS','','');
-INSERT INTO "Account" VALUES(9,'','Dr. Deanna David','','');
-INSERT INTO "Account" VALUES(10,'','Kerry Franco V','','');
-INSERT INTO "Account" VALUES(11,'','Sample Account for Entitlements','','');
+INSERT INTO "Account" VALUES(1,'','Sample Account for Entitlements','','');
+INSERT INTO "Account" VALUES(2,'','Faith Santos DDS','','');
+INSERT INTO "Account" VALUES(3,'','Mr. Elijah Herrera DVM','','');
+INSERT INTO "Account" VALUES(4,'','Ms. Debra Jacobson PhD','','');
+INSERT INTO "Account" VALUES(5,'','Tricia Melendez','','');
+INSERT INTO "Account" VALUES(6,'','Elizabeth Barajas DDS','','');
+INSERT INTO "Account" VALUES(7,'','Mrs. Rose Barnett DDS','','');
+INSERT INTO "Account" VALUES(8,'','Mr. Travis Taylor II','','');
+INSERT INTO "Account" VALUES(9,'','Fernando Navarro DDS','','');
+INSERT INTO "Account" VALUES(10,'','Dr. Deanna David','','');
+INSERT INTO "Account" VALUES(11,'','Kerry Franco V','','');
+INSERT INTO "Account" VALUES(12,'','Sample Account for Entitlements','','');
 CREATE TABLE "Application__c" (
 	id INTEGER NOT NULL, 
 	"Amount__c" VARCHAR(255), 
 	"Date__c" VARCHAR(255), 
 	PRIMARY KEY (id)
 );
-INSERT INTO "Application__c" VALUES(1,'5000.0','2023-03-09');
-INSERT INTO "Application__c" VALUES(2,'15000.0','2023-02-23');
-INSERT INTO "Application__c" VALUES(3,'20000.0','2023-02-25');
-INSERT INTO "Application__c" VALUES(4,'10000.0','2023-03-08');
+INSERT INTO "Application__c" VALUES(1,'15000.0','2023-02-23');
+INSERT INTO "Application__c" VALUES(2,'20000.0','2023-02-25');
+INSERT INTO "Application__c" VALUES(3,'10000.0','2023-03-08');
+INSERT INTO "Application__c" VALUES(4,'5000.0','2023-03-09');
+INSERT INTO "Application__c" VALUES(5,'5000.0','2023-04-06');
 CREATE TABLE "Delivery_Item__c" (
 	id INTEGER NOT NULL, 
 	"Food_Expiration__c" VARCHAR(255), 
@@ -54,7 +56,7 @@ CREATE TABLE "DeploymentFrequency__c" (
 	"Application_Account__c" VARCHAR(255), 
 	PRIMARY KEY (id)
 );
-INSERT INTO "DeploymentFrequency__c" VALUES(1,'BB','','47000.0','BACGHIJ','Planned','','10');
+INSERT INTO "DeploymentFrequency__c" VALUES(1,'BB','','47000.0','JBACIGH','Planned','','11');
 CREATE TABLE "Opportunity" (
 	id INTEGER NOT NULL, 
 	"Name" VARCHAR(255), 
@@ -64,14 +66,14 @@ CREATE TABLE "Opportunity" (
 	"StageName" VARCHAR(255), 
 	PRIMARY KEY (id)
 );
-INSERT INTO "Opportunity" VALUES(1,'J','10','2000.0','2023-02-24','Prospecting');
-INSERT INTO "Opportunity" VALUES(2,'F','8','10000.0','2023-02-25','Prospecting');
-INSERT INTO "Opportunity" VALUES(3,'E','9','5000.0','2023-02-25','Prospecting');
-INSERT INTO "Opportunity" VALUES(4,'D','9','10000.0','2023-02-25','Prospecting');
-INSERT INTO "Opportunity" VALUES(5,'B','10','10000.0','2023-02-25','Prospecting');
-INSERT INTO "Opportunity" VALUES(6,'A','10','10000.0','2023-02-25','Prospecting');
-INSERT INTO "Opportunity" VALUES(7,'C','10','10000.0','2023-02-25','Prospecting');
-INSERT INTO "Opportunity" VALUES(8,'I','10','5000.0','2023-02-26','Prospecting');
-INSERT INTO "Opportunity" VALUES(9,'G','10','5000.0','2023-02-25','Prospecting');
-INSERT INTO "Opportunity" VALUES(10,'H','10','5000.0','2023-02-24','Prospecting');
+INSERT INTO "Opportunity" VALUES(1,'J','11','2000.0','2023-02-24','Prospecting');
+INSERT INTO "Opportunity" VALUES(2,'B','11','10000.0','2023-02-25','Prospecting');
+INSERT INTO "Opportunity" VALUES(3,'A','11','10000.0','2023-02-25','Prospecting');
+INSERT INTO "Opportunity" VALUES(4,'C','11','10000.0','2023-02-25','Prospecting');
+INSERT INTO "Opportunity" VALUES(5,'I','11','5000.0','2023-02-26','Prospecting');
+INSERT INTO "Opportunity" VALUES(6,'G','11','5000.0','2023-02-25','Prospecting');
+INSERT INTO "Opportunity" VALUES(7,'H','11','5000.0','2023-02-24','Prospecting');
+INSERT INTO "Opportunity" VALUES(8,'F','9','10000.0','2023-02-25','Prospecting');
+INSERT INTO "Opportunity" VALUES(9,'E','10','5000.0','2023-02-25','Prospecting');
+INSERT INTO "Opportunity" VALUES(10,'D','10','10000.0','2023-02-25','Prospecting');
 COMMIT;
